897471f88b2be0764cd59d2e152b50cc7bda7b36
nakita628 <nakita@ypm.llc>
2025-07-09 04:08:39 +0000

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['**/*.md', './.vitepress/**/*.vue'],
+  content: ['**/*.md', './.vitepress/**/*.vue', '!**/node_modules/**' ],
   theme: {
     extend: {},
   },
